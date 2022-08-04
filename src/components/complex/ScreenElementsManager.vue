98c6dfc37dcb7d553825ed7ce80d97f09e54bb48
Filip Drygaś <tejotemku@gmail.com>
2022-08-04 13:23:52 +0200

    created Screen Elements Manager for project manager

---
+++ src/components/complex/ScreenElementsManager.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="element-box" >
+    <huge> Layers </huge>
+    <div class="element-list" >
+      <div v-for="element in screenElements" :key="element.id">
+        {{ element.description }}
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+
+export default {
+  name: 'ScreenElementsManager',
+  props: ['screenElements'],
+  mounted() {
+    this.screenElements.sort((a,b) => a.depth - b.depth);
+  }
+}
+</script>
+<style scoped>
+.element-box {
+  background-color: aqua;
+}
+
+.element-list {
+  overflow-y: scroll;
+}
+</style>
--- src/views/user/ProjectEditor.vue
+++ src/views/user/ProjectEditor.vue
@@ -1,19 +1,90 @@
 <template>
   <div>
     <LoggedInNavbarVue />
-    Project Editor site - project id {{this.$route.params.id}}
-  </div>
+    <ScreenElementsManager :screenElements="projectData.screens[currentScreenId].elements" />
+    <!-- Project Editor site - project id {{this.$route.params.id}} -->
 
+    <!-- TODO: Project view -->
+    <!-- TODO: Assets -->
+    <!-- TODO: Library -->
+    <!-- TODO: Pages -->
+    <!-- TODO: Layers -->
+  </div>
 </template>
 
 <script>
 import LoggedInNavbarVue from '../../components/elementary/LoggedInNavbar.vue'
+import ScreenElementsManager from '../../components/complex/ScreenElementsManager.vue'
 
 
 export default {
   name: "ProjectEditor",
   components: {
-    LoggedInNavbarVue
+    LoggedInNavbarVue,
+    ScreenElementsManager
+  },
+  data() {
+    return {
+      projectData: null,
+      currentScreenId: 0
+    }
+  },
+  mounted() {
+    try {
+      this.projectData = this.fetchData();
+    }
+    catch(e) {
+      console.log(e);
+    }
+  },
+  methods: {
+    fetchData() {
+      // let projectId = this.$route.params.id;
+      // TODO: this is a mock
+
+      return {
+        screens: [
+          {
+            id: 0,
+            name: "Mock Screen 1",
+            elements: [
+              {
+                id: 0,
+                depth: 0,
+                description: "Button - 1",
+                class: "btn btn-primary",
+                properties: {
+                  text: "Click Me #1"
+                }
+              },
+              {
+                id: 1,
+                depth: 2,
+                description: "Button - 2",
+                class: "btn btn-secondary",
+                properties: {
+                  text: "Click Me #2"
+                }
+              },
+              {
+                id: 2,
+                depth: 1,
+                description: "Email Field - 2",
+                class: "form-control",
+                properties: {
+                  text: "Click Me #2"
+                }
+              },
+            ]
+          }
+        ],
+        title: "Mock Project Name",
+        assets: []
+      }
+    },
+    printProjectData() {
+      console.log(this.projectData);
+    }
   }
 }
 </script>
