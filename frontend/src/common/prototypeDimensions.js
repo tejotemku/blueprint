1c5c29d64b53131f60d562dd53fe2f77df50e192
Filip Drygaś <tejotemku@gmail.com>
2022-11-08 18:06:12 +0100

    new screen dimensions

--- frontend/src/common/prototypeDimensions.js
+++ frontend/src/common/prototypeDimensions.js
@@ -1,11 +1,23 @@
 export const windowDimensions = [
   '1920x1080(16:9)',
+  '2560x1440(16:9)',
+  '1920x1440(4:3)',
+  '1680x1050(16:10)',
   '800x600(4:3)',
   '1280x720(16:9)',
   '1536x864(16:9)',
   '1440x900(16:10)',
   '1366x768(16:9)',
   '1600x900(16:9)',
+  '390x844(custom) iPhone 13',
+  '428x926(custom) iPhone 13 Pro Max',
+  '411x869(custom) Pixel 4 / Pixel 4 XL',
+  '360x760(9:19) Samsung S10',
+  '412x869(custom) Samsung S10+',
+  '1368x912(3:2) Ms Surface Pro 6',
+  '1024x768(4:3) Android Tablet 9"',
+  '960x600(16:10) Android Tablet 7"',
+  '1080x810(4:3) iPad 10.2"',
 ];
 
 export const defaultWindowDimensions = '1920x1080(16:9)';
